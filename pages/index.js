27f09ed75f275b208fc9664e4bcef3b02cc7492e
SaadiaELF <saadiaelfekak@gmail.com>
2023-03-31 03:39:04 +0100

    setting up colors

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="font-barlow text-3xl">Saadia El fekak</h1>
-      <h2 className="font-montserrat text-3xl">Saadia El fekak</h2>
+      <h1 className="bg-primary_1 font-barlow text-3xl text-secondary_2">
+        Saadia El fekak
+      </h1>
+      <h2 className="bg-primary_2 font-montserrat text-3xl text-secondary_1">
+        Saadia El fekak
+      </h2>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,12 @@ export const theme = {
       barlow: ["var(--font-barlow)", ...defaultTheme.fontFamily.mono],
       montserrat: ["var(--font-montserrat)", ...defaultTheme.fontFamily.sans],
     },
+    colors: {
+      primary_1: "#222831",
+      primary_2: "#393E46",
+      secondary_1:'#00ADB5',
+      secondary_2:'#00FFF5'
+    },
   },
 };
 export const plugins = [];
