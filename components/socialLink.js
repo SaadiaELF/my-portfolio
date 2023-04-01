46ea7c6b42f1b47ea38a8d2580e2b10e5531d6b4
SaadiaELF <saadiaelfekak@gmail.com>
2023-04-01 21:29:25 +0100

    deleted borders

--- components/socialLink.js
+++ components/socialLink.js
@@ -4,7 +4,7 @@ export default function socialLink({ details }) {
     <a
       href={details.link}
       target="_blank"
-      className="rounded-md border border-solid border-primary_2 bg-primary_1 px-5 py-1.5 text-sm text-secondary_2 shadow-sm hover:bg-primary_2 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary_2"
+      className="rounded-md px-5 py-1.5 text-sm text-secondary_2 shadow-sm hover:bg-primary_2 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary_2"
     >
       <FontAwesomeIcon icon={details.icon} style={{ color: "#00fff5" }} />
       <span className="ps-1.5 font-barlow text-lg">{details.text}</span>
