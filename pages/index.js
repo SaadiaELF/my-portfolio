e22b820d1d1ebed5cd87f63cf33789def5bfa8e2
SaadiaELF <saadiaelfekak@gmail.com>
2023-04-01 01:19:46 +0100

    added navbar

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,8 @@
+import Navbar from "../components/navbar";
 export default function Home() {
   return (
     <div>
-      <h1 className="bg-primary_1 font-barlow text-3xl text-secondary_2">
-        Saadia El fekak
-      </h1>
-      <h2 className="bg-primary_2 font-montserrat text-3xl text-secondary_1">
-        Saadia El fekak
-      </h2>
+      <Navbar />
     </div>
   );
 }
