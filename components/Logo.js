a399de190a644d46ce2feaa143fffa6375b73812
SaadiaELF <saadiaelfekak@gmail.com>
2023-04-04 22:02:26 +0100

    added logo component

---
+++ components/Logo.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Logo({ src, alt }) {
+  return (
+    <Image
+      className="col-span-2 max-h-20 w-full object-contain brightness-100 motion-safe:animate-pulse lg:col-span-1"
+      src={src}
+      alt={alt}
+    />
+  );
+}
