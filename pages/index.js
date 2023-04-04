5a283af936d51297decc495bf564902d848ff0f3
SaadiaELF <saadiaelfekak@gmail.com>
2023-04-04 17:46:18 +0100

    added about page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Hero from "../components/hero";
 import Navbar from "../components/navbar";
+import About from "./about";
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <About />
     </div>
   );
 }
