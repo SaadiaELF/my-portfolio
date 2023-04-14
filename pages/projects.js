74195c005230476f0457745fea3f145155393406
SaadiaELF <saadiaelfekak@gmail.com>
2023-04-14 17:19:20 +0100

    added page structure

--- pages/projects.js
+++ pages/projects.js
@@ -1,10 +1,12 @@
 import Container from "../components/container";
 import Heading from "../components/heading";
+import ProjectsContainer from "../components/projectsContainer";
 
 export default function Projects() {
   return (
     <Container id="projects" bg_class="bg-primary_2">
       <Heading text="Projects" />
+      <ProjectsContainer />
     </Container>
   );
 }
