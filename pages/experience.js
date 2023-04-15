241ce700104e94def3236d5548eb9e0d76109492
SaadiaELF <saadiaelfekak@gmail.com>
2023-04-15 11:15:42 +0100

    changed background

--- pages/experience.js
+++ pages/experience.js
@@ -4,7 +4,7 @@ import Heading from "../components/heading";
 
 export default function Experience() {
   return (
-    <Container id="experience" bg_class="bg-primary_1">
+    <Container id="experience" bg_class="bg-primary_1 xl:bg-main">
       <Heading text="Experience" />
       <ExpContainer />
     </Container>
