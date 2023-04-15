1fa20ce2e15a925a169206bb4d63f7991b45942c
SaadiaELF <saadiaelfekak@gmail.com>
2023-04-15 07:36:27 +0100

    added projects data

--- components/oneProject.js
+++ components/oneProject.js
@@ -1,22 +1,17 @@
 import Image from "next/image";
-import feedbackTrackerImg from "../public/projects/cyf-tracker.png";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 
-export default function OneProject() {
+export default function OneProject({ project }) {
   return (
-    <div className="mt-10 grid h-max min-w-full gap-x-3 gap-y-2 sm:h-80 sm:snap-center sm:grid-cols-2 sm:grid-rows-4 ">
+    <div className="mt-10 grid h-max min-w-full gap-x-3 gap-y-2 sm:h-80 sm:grid-cols-2 sm:grid-rows-4 ">
       <div className="flex items-center justify-between p-2">
         <h3 className="font-barlow text-2xl tracking-tight text-secondary_1 sm:text-3xl ">
-          CYF_Feedback Tracker
+          {project.title}
         </h3>
         <div className="flex items-center">
-          <a
-            href="https://github.com/SaadiaELF/wm4-1-techieelites-feedbacktracker.git"
-            target="_blank"
-            className="h-fit px-2"
-          >
+          <a href={project.github} target="_blank" className="h-fit px-2">
             <FontAwesomeIcon
               className="icon"
               icon={faGithub}
@@ -24,11 +19,7 @@ export default function OneProject() {
               style={{ color: "#00ADB5" }}
             />
           </a>
-          <a
-            href="https://starter-kit-5gb3.onrender.com"
-            target="_blank"
-            className="h-fit px-2"
-          >
+          <a href={project.url} target="_blank" className="h-fit px-2">
             <FontAwesomeIcon
               className="icon"
               icon={faUpRightFromSquare}
@@ -38,34 +29,26 @@ export default function OneProject() {
           </a>
         </div>
       </div>
-      <div className="snap-always ">
-        <Image
-          className="w-contain max-w-60 h-max drop-shadow-md grayscale hover:filter-none sm:h-80"
-          src={feedbackTrackerImg}
-          alt="project"
-        />
-      </div>
-      <div className="flex w-full snap-always flex-col justify-center bg-primary_1 p-4 sm:row-span-3  ">
+      <Image
+        className="w-contain max-w-60 h-max drop-shadow-md grayscale hover:filter-none sm:h-80"
+        src={project.image}
+        alt="project"
+      />
+      <div className="flex w-full  flex-col justify-center bg-primary_1 p-4 sm:row-span-3  ">
         <p className="select-none bg-primary_1 text-justify font-montserrat text-base leading-6 text-teal-50 antialiased">
-          The Feedback Tracker App is a powerful tool designed to help students
-          and mentors alike to track progress and provide valuable feedback
-          throughout a course. With this app, students can easily send feedback
-          about their progress, while mentors can add their own feedback about
-          how their students are doing.
+          {project.description}
         </p>
-        <div className="mt-4">
-          <span className="me-2 rounded-lg bg-primary_2 px-2 py-1 text-justify font-montserrat text-sm leading-6 text-secondary_1 antialiased">
-            React
-          </span>
-          <span className="me-2 rounded-lg bg-primary_2  px-2 py-1 text-justify font-montserrat text-sm leading-6 text-secondary_1 antialiased">
-            MaterialUI
-          </span>
-          <span className="me-2 rounded-lg bg-primary_2  px-2 py-1 text-justify font-montserrat text-sm leading-6 text-secondary_1 antialiased">
-            Node
-          </span>
-          <span className="me-2 rounded-lg bg-primary_2  px-2 py-1 text-justify font-montserrat text-sm leading-6 text-secondary_1 antialiased">
-            Express
-          </span>
+        <div className="mt-2 flex flex-wrap">
+          {project.tech_stack.map((tech, i) => {
+            return (
+              <span
+                key={i}
+                className="m-1 rounded-lg bg-primary_2 px-2 py-1 font-montserrat text-sm text-secondary_1 antialiased"
+              >
+                {tech}
+              </span>
+            );
+          })}
         </div>
       </div>
     </div>
--- components/projectsContainer.js
+++ components/projectsContainer.js
@@ -1,11 +1,12 @@
 import OneProject from "./oneProject";
+import projects from "../data/projects.js";
 
 export default function ProjectsContainer() {
   return (
-    <div className="flex flex-col justify-center overflow-scroll sm:snap-x sm:snap-mandatory sm:flex-row">
-      <OneProject />
-      <OneProject />
-      <OneProject />
+    <div className="flex flex-col justify-center gap-4">
+      {projects.map((project, i) => {
+        return <OneProject key={i} project={project} />;
+      })}
     </div>
   );
 }
