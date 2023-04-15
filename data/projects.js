950f6b7d22f03ef7beaaf8e9b62a3a953dbff216
SaadiaELF <saadiaelfekak@gmail.com>
2023-04-15 11:15:10 +0100

    Added new data

--- data/projects.js
+++ data/projects.js
@@ -1,4 +1,6 @@
 import feedbackTrackerImg from "../public/projects/cyf-tracker.png";
+import countriesImg from "../public/projects/countries.png";
+import tvShowsImg from "../public/projects/tv-shows.png";
 
 const projects = [
   {
@@ -8,28 +10,26 @@ const projects = [
     url: "https://starter-kit-5gb3.onrender.com",
     image: feedbackTrackerImg,
     description:
-      " The Feedback Tracker App is a powerful tool designed to help students and mentors alike to track progress and provide valuable feedback throughout a course. With this app, students can easily send feedback about their progress, while mentors can add their own feedback about how their students are doing.",
+      "The Feedback Tracker App is a powerful tool designed to help students and mentors alike to track progress and provide valuable feedback throughout a course. With this app, students can easily send feedback about their progress, while mentors can add their own feedback about how their students are doing.",
     tech_stack: ["React", "Material UI", "NodeJS", "ExpressJS", "Postgresql"],
   },
   {
-    title: "CYF_Feedback Tracker",
-    github:
-      "https://github.com/SaadiaELF/wm4-1-techieelites-feedbacktracker.git",
-    url: "https://starter-kit-5gb3.onrender.com",
-    image: feedbackTrackerImg,
+    title: "Countries",
+    github: "https://github.com/SaadiaELF/countries-react.git",
+    url: "https://cyf-saadiaelf-countries-react.netlify.app/",
+    image: countriesImg,
     description:
-      " The Feedback Tracker App is a powerful tool designed to help students and mentors alike to track progress and provide valuable feedback throughout a course. With this app, students can easily send feedback about their progress, while mentors can add their own feedback about how their students are doing.",
-    tech_stack: ["React", "Material UI", "NodeJS", "ExpressJS", "Postgresql"],
+      "This is a React-based web application that provides information on countries around the world. With this app, you can search for a country, view its basic information, such as its population and capital city. You can also filter countries by region.",
+    tech_stack: ["React"],
   },
   {
-    title: "CYF_Feedback Tracker",
-    github:
-      "https://github.com/SaadiaELF/wm4-1-techieelites-feedbacktracker.git",
-    url: "https://starter-kit-5gb3.onrender.com",
-    image: feedbackTrackerImg,
+    title: "TV Shows",
+    github: "https://github.com/SaadiaELF/tv-show-dom-project.git",
+    url: "https://cyf-saadiaelf-tv.netlify.app/",
+    image: tvShowsImg,
     description:
-      " The Feedback Tracker App is a powerful tool designed to help students and mentors alike to track progress and provide valuable feedback throughout a course. With this app, students can easily send feedback about their progress, while mentors can add their own feedback about how their students are doing.",
-    tech_stack: ["React", "Material UI", "NodeJS", "ExpressJS", "Postgresql"],
+      "The TV show project that utilizes CSS, HTML, JavaScript, and DOM manipulation is a highly functional and impressive application. The application provides users with the ability to display TV shows, view lists of episodes, and search for shows and episodes based on the TV Maze API.",
+    tech_stack: ["HTML", "CSS", "Javascript"],
   },
 ];
 
