6ce5eae82ee9d7fd34a69487b8a83946883e217a
SaadiaELF <saadiaelfekak@gmail.com>
2023-05-22 09:43:04 +0100

    moved logos to data

---
+++ data/logos.js
@@ -0,0 +1,25 @@
+import JsLogo from "../public/logos/js.svg";
+import CssLogo from "../public/logos/css.svg";
+import HtmlLogo from "../public/logos/html.svg";
+import ReactLogo from "../public/logos/react.svg";
+import NodeLogo from "../public/logos/node.png";
+import PostgresSqlLogo from "../public/logos/postgresql.png";
+import MongoDBLogo from "../public/logos/mongoDB.png";
+import TailwindLogo from "../public/logos/tailwind.png";
+import MaterialLogo from "../public/logos/material-ui.svg";
+import BootstrapLogo from "../public/logos/bootstrap.svg";
+
+const logos = [
+  { img: HtmlLogo, alt: "HTML logo" },
+  { img: CssLogo, alt: "CSS logo" },
+  { img: JsLogo, alt: "JS logo" },
+  { img: ReactLogo, alt: "React logo" },
+  { img: NodeLogo, alt: "Node logo" },
+  { img: PostgresSqlLogo, alt: "Postgresql logo" },
+  { img: MongoDBLogo, alt: "MongoDb logo" },
+  { img: TailwindLogo, alt: "Tailwind logo" },
+  { img: MaterialLogo, alt: "Material UI logo" },
+  { img: BootstrapLogo, alt: "Bootstrap logo" },
+];
+
+export default logos;
--- pages/about.js
+++ pages/about.js
@@ -2,29 +2,9 @@ import Image from "next/image";
 import Container from "../components/container";
 import Heading from "../components/heading";
 import Logo from "../components/Logo";
-import JsLogo from "../public/logos/js.svg";
-import CssLogo from "../public/logos/css.svg";
-import HtmlLogo from "../public/logos/html.svg";
-import ReactLogo from "../public/logos/react.svg";
-import NodeLogo from "../public/logos/node.png";
-import PostgresSqlLogo from "../public/logos/postgresql.png";
-import MongoDBLogo from "../public/logos/mongoDB.png";
-import TailwindLogo from "../public/logos/tailwind.png";
-import MaterialLogo from "../public/logos/material-ui.svg";
-import BootstrapLogo from "../public/logos/bootstrap.svg";
 import profileImg from "../public/saadia.jpeg";
-const logos = [
-  { img: HtmlLogo, alt: "HTML logo" },
-  { img: CssLogo, alt: "CSS logo" },
-  { img: JsLogo, alt: "JS logo" },
-  { img: ReactLogo, alt: "React logo" },
-  { img: NodeLogo, alt: "Node logo" },
-  { img: PostgresSqlLogo, alt: "Postgresql logo" },
-  { img: MongoDBLogo, alt: "MongoDb logo" },
-  { img: TailwindLogo, alt: "Tailwind logo" },
-  { img: MaterialLogo, alt: "Material UI logo" },
-  { img: BootstrapLogo, alt: "Bootstrap logo" },
-];
+import logos from "../data/logos.js";
+
 export default function About() {
   return (
     <Container id="about" bg_class="bg-primary_2">
